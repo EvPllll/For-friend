70483ca26f513c1700486b7965cff35341feeae1
EvPllll <eugeneplyusnin@yandex.ru>
2024-05-17 10:06:54 +0700

    fix message for showing

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const button = document.querySelector('button');
 const output = document.querySelector('.output');
 
 button.addEventListener('click', function() {
-    output.innerHTML = "ТЫ ВОНЮЧКА!!!"
+    output.innerHTML = "ТЫ ВОНЮЧКА!!! Желаю удачного дня! :)"
 });
